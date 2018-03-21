d40b323267e744dfdc7dc93871dfa84d72ee0251
Rasmus <Botched135>
2018-03-21 13:56:51 +0100

    agent files

---
+++ adaptive/adam_w.c
@@ -0,0 +1,25 @@
+//===========================================================================
+//
+// Name:			tig_w.c
+// Function:		
+// Programmer:		
+// Last update:		
+// Tab Size:		4 (real tabs)
+//===========================================================================
+
+
+#include "inv.h"
+
+#define W_GAUNTLET				15
+#define W_SHOTGUN				100
+#define W_MACHINEGUN			20
+#define W_GRENADELAUNCHER		40
+#define W_ROCKETLAUNCHER		190
+#define W_RAILGUN				180
+#define W_BFG10K				95
+#define W_LIGHTNING				280
+#define W_PLASMAGUN				190
+#define W_GRAPPLE				15
+
+//
+#include "fw_weap.c"
